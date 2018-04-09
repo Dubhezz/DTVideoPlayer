71dc4cb7058c65d28d5e0d6d77319c12c16c74e4
mydubhe <mydubhe@sina.com>
2018-04-09 18:38:11 +0800

    Initial Commit

---
+++ DTVideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DTVideoPlayer
+//
+//  Created by dubhe on 2018/4/9.
+//  Copyright © 2018年 Dubhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DTVideoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DTVideoPlayer
+//
+//  Created by dubhe on 2018/4/9.
+//  Copyright © 2018年 Dubhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
